03215bc3d9cae5cc97f6848156afc6c913e5bf65
Trinh Duc Tai <trinhductai6869@gmail.com>
2019-11-26 16:26:17 +0700

    xoa het

--- test/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const {src, dest} = require('gulp');
-const sass = require('gulp-sass');
-function compiler(){
-    return src('./test.scss')
-            .pipe(sass())
-            .pipe(dest('./css'))
-}
-exports.scss = compiler  
